951122238893286bc39cb751867bf1960eb94f14
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2019-09-18 22:47:42 +0800

    :wrench: update vuepress config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,10 +11,12 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon.png` }],
     ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/mstile-150x150.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ['script', { src: "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" }],
+    ['script', {}, '(adsbygoogle = window.adsbygoogle || []).push({  google_ad_client: "ca-pub-8586652723015758",  enable_page_level_ads: true });']
   ],
   locales: {
     '/': {
