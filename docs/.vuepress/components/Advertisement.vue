bcdce80b93de9d458d516883a9c1de2df802dfb2
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2021-02-27 00:28:05 +0800

    :sparkles: uodate advertisement.

--- docs/.vuepress/components/Advertisement.vue
+++ docs/.vuepress/components/Advertisement.vue
@@ -27,25 +27,9 @@ export default {
       carouselHeight: '100px',
       items: [
         {
-          path: '//click.dji.com/AAMD9H5ODH2wTVtxUWwxxw?pm=link&as=0004',
-          image:
-            '//u.djicdn.com/uploads/ad_image_file/file/1670/%E5%A4%A7%E7%96%86%E6%8E%A8%E5%B9%BF%E8%81%94%E7%9B%9Fbanner_970-250.jpg',
-          alt: '“御”Mavic 2 - 全新 Hasselblad 镜头与你一同高瞻远瞩，以小见大'
-        },
-        {
-          path: '//click.dji.com/AHT8n4-15xfguKCaOzzEPA?pm=link&as=0004',
-          image: '//u.djicdn.com/uploads/ad_image_file/file/1234/970_250.jpg',
-          alt: '特洛Tello-迷你无人机'
-        },
-        {
-          path: '//click.dji.com/AHT8n4-15xfguKCaOzzEPA?pm=link&as=0004',
-          image: '//u.djicdn.com/uploads/ad_image_file/file/1242/970_250.jpg',
-          alt: '特洛Tello-益智编程无人机'
-        },
-        {
-          path: '//click.dji.com/AHT8n4-15xfguKCaOzzEPA?pm=link&as=0004',
-          image: '//u.djicdn.com/uploads/ad_image_file/file/992/970___250.jpg',
-          alt: '大疆无人机-DJI "御" Mavic Air'
+          path: 'https://nicelinks.site/?utm_source=lovejade.cn',
+          image: 'https://s1.ax1x.com/2020/06/13/tvs7NQ.png',
+          alt: '倾城之链 - 云集全球优秀网站'
         }
       ]
     }
